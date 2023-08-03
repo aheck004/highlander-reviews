d49a2f6fac53f3afb799daf96f1039fd1efda62b
jalvarez957 <87673004+KilloPillers@users.noreply.github.com>
2023-08-03 13:21:19 -0700

    Provided server scripts to get data from UCR course catalog
    -added skeletons to class page
    -course descriptions and titles are now viewable in class page

---
+++ server/server_scripts/cURL_UCR_Course_Catalog.py
@@ -0,0 +1,109 @@
+import requests
+from pymongo import MongoClient, UpdateOne  # pip3 install pymongo
+import math
+from tqdm import tqdm
+import json
+import re
+
+def fixCourseTitle(courseTitle):
+    courseTitle = courseTitle.lower()
+    courseTitle = courseTitle.capitalize()
+    def capitalize_word(match):
+        word = match.group()
+        if word.lower() not in ['a', 'is', 'in', 'an', 'at', 'and', 'the', 'for', 'of', 'to', 'by']:
+            return word.capitalize()
+        return word
+    return re.sub(r'(?<=\s)\w+\b', capitalize_word, courseTitle)
+
+
+pageMaxSize = 500
+JSESSIONID="5BE2A6BE006821F6FB9092ADC67E1E43"
+
+url = 'https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/courseSearchResults/'
+course_search_results_header = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Sec-Fetch-Site': 'same-origin',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Sec-Fetch-Mode': 'cors',
+    'Host': 'registrationssb.ucr.edu',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
+    'Referer': 'https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/courseSearch/courseSearch',
+    'Connection': 'keep-alive',
+    'Cookie': f'JSESSIONID={JSESSIONID}; BIGipServerp_registrationssb.ucr.edu_8443=2811498668.64288.0000',
+    'Sec-Fetch-Dest': 'empty',
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-Synchronizer-Token': '10e3d3f3-2370-4899-aa44-2802a8f28cbb'
+}
+
+params = {
+    'txt_term': '202410',
+    'startDatepicker': '',
+    'endDatepicker': '',
+    'uniqueSessionId': 'xbtch1691017523147',
+    'pageOffset': '0',
+    'pageMaxSize': pageMaxSize,
+    'sortColumn': 'subjectDescription',
+    'sortDirection': 'asc'
+}
+
+course_data_list = []
+total_entries = None
+
+response = requests.get(url+"courseSearchResults", headers=course_search_results_header, params=params)
+if response.status_code == 200:
+    data = response.json()
+    total_entries = data['totalCount']
+    print(f"Total entries: {total_entries}")
+
+    course_data_list.extend(data['data'])
+else:
+    print(f"Request failed with status code: {response.status_code}")
+
+total_pages = math.ceil(total_entries/500)
+with tqdm(total=total_pages, desc="Requesting Courses", unit="page", initial=1) as pbar:
+    for page_offset in range(1, total_pages):
+        params = {
+            'txt_term': '202410',
+            'startDatepicker': '',
+            'endDatepicker': '',
+            'uniqueSessionId': 'xbtch1691017523147',
+            'pageOffset': page_offset*pageMaxSize,
+            'pageMaxSize': '500',
+            'sortColumn': 'subjectDescription',
+            'sortDirection': 'asc'
+        }
+        response = requests.get(url, headers=course_search_results_header, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            course_data_list.extend(data['data'])
+            pbar.update()
+        else: 
+            print(f"Request failed with status code: {response.status_code}")
+
+with tqdm(total=len(course_data_list), desc="Requesting Descriptions", unit="request") as pbar:
+    with open("./config.json") as config_file:
+        config = json.load(config_file)
+    client = MongoClient(config["URI"])
+    db = client['UCR']
+    collection = db['courses']
+    for course in course_data_list:
+        if '-' in course["courseNumber"] or course["courseDescription"] == "For college use only" or course["courseDescription"] == "null":
+            pbar.update()
+            continue
+        data = requests.post(url+"getCourseDescription", data={'term': '202410', 'subjectCode': course["subjectCode"], 'courseNumber': course["courseNumber"]})
+        course["courseDescription"] = data.text
+        query = {'class_name': course["subjectCode"]+course["courseNumber"]}
+        update_operation = {'$set': {
+            "class_name": course["subjectCode"]+course["courseNumber"], 
+            "subject_code": course["subjectCode"], 
+            "course_number": course["courseNumber"],
+            "course_diff": 0,
+            "course_title": fixCourseTitle(course["courseTitle"]),
+            "course_description": course["courseDescription"].strip('\n')
+        }}
+        collection.update_one(query, update_operation, upsert=True)
+        pbar.update()
+
+with open("courses_data.json", 'w') as file:
+    json.dump({"data": course_data_list}, file, indent=4)
--- src/components/classPage.js
+++ src/components/classPage.js
@@ -4,7 +4,7 @@ import DifficultyGraph from "./DifficultyGraph";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import CreateReviewModal from "./CreateReview";
-import { Box, Paper, Typography, Divider, Chip, Button } from "@mui/material";
+import { Box, Paper, Typography, Divider, Chip, Button, Skeleton } from "@mui/material";
 import CourseSlider from "./CourseSlider";
 import SortButton from "./SortButton";
 import PrimarySearchAppBar from "./Header";
@@ -18,7 +18,7 @@ import getGoogleOAuthURL from "../getGoogleURL.js";
 function ClassPage() {
   const [reviews, setReviews] = useState([]);
   const [graphData, setGraphData] = useState([0, 0, 0, 0, 0]);
-  const [averageDiff, setAverageDiff] = useState(0);
+  const [courseData, setCourseData] = useState(null)
   const { subjectCode } = useParams();
   const { courseNumber } = useParams();
   const course = subjectCode + courseNumber;
@@ -44,7 +44,8 @@ function ClassPage() {
     axios
       .get(process.env.REACT_APP_NODE_SERVER + `/get-course/${course}`)
       .then((response) => {
-        setAverageDiff(response.data[0].average_diff);
+        console.log(response.data[0])
+        setCourseData(response.data[0])
       });
   }, [subjectCode, courseNumber]);
 
@@ -62,61 +63,120 @@ function ClassPage() {
           flexDirection: "column",
         }}
       >
-        <PrimarySearchAppBar key={course} title={course} />
+        <PrimarySearchAppBar title={courseData ? courseData.class_name : null} />
         <Box
           className="course-hero"
           sx={{
             display: "flex",
             gap: "20px",
             flexWrap: "wrap",
+            flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
             margin: "10px",
           }}
         >
+          {courseData ?
+            <Typography variant="h2" align="center" color="text.main">
+              {courseData.class_name}
+            </Typography>
+          :
+            <Skeleton variant="rectangular" width={210} height={118} />
+          }
+          
+          {courseData ? 
+          <Typography variant="h2"align="center" color="text.main">              
+            {courseData.course_title}
+          </Typography>
+          :
+            <Skeleton variant="rectangular" width={210} height={118} />
+          }
           <Box
-            className="course-hero-left"
             sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+              display:"flex",
+              gap: 10
             }}
           >
-            <Typography variant="h1" align="center" color="text.main">
-              {subjectCode} {courseNumber}
-            </Typography>
-            <Typography variant="h3" color="text.main">
-              {parseFloat(averageDiff / 2).toFixed(2)}/5
-            </Typography>
-            {googleUser ? (
-              <CreateReviewModal
-                total_reviews={reviews.length}
-                avg_diff={averageDiff}
-              />
-            ) : (
-              <Button
-                endIcon={<GoogleIcon />}
-                variant="contained"
-                color="primary"
-                href={getGoogleOAuthURL(window.location.href)}
-              >
-                Login to Review
-              </Button>
-            )}
-          </Box>
-          <Box className="course-hero-right">
-            <Paper
+            <Box
+              className="course-hero-left"
               sx={{
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                bgcolor: theme.palette.secondary.main
+                width: 350,
+                gap: 5
               }}
             >
-              <DifficultyGraph review_data={graphData} />
-            </Paper>
-            <Box key={subjectCode}>
-              <CourseSlider subject={subjectCode} />
+              {courseData ? 
+              <Box>
+                <Typography sx={{
+                  alignSelf: 'start',
+                  fontWeight: 'bold'
+                }}variant="body1" color="text.main">
+                  Course Description:
+                </Typography>
+                <Typography variant="subtitle1" color="text.main" fontSize={'.9rem'}>
+                 {courseData.course_description} 
+                </Typography>
+              </Box>
+              : <Skeleton variant="rectangular" width={350} height={300}/>
+              }
+              {googleUser && courseData ? (
+                <CreateReviewModal
+                  total_reviews={reviews.length}
+                  avg_diff={courseData.average_diff}
+                />
+              ) : (
+                <Button
+                  endIcon={<GoogleIcon />}
+                  variant="contained"
+                  color="primary"
+                  href={getGoogleOAuthURL(window.location.href)}
+                >
+                  Login to Review
+                </Button>
+              )}
+            </Box>
+            <Box className="course-hero-right">
+              <Paper
+                sx={{
+                  position: 'relative',
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  bgcolor: theme.palette.secondary.main,
+                  flexDirection: "column-reverse",
+                }}
+              >
+                {courseData ? 
+                <>
+                <DifficultyGraph review_data={graphData} />
+                  <Box sx={{
+                    position:'absolute',
+                    top: '10px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                  }}>
+                    <Typography color="text.main">
+                        Average Difficulty
+                    </Typography>
+                    <Typography variant="h2" color="text.main">
+                      {parseFloat(courseData.average_diff / 2).toFixed(2)}/5
+                    </Typography>
+                  </Box>
+                  </>
+                  : 
+                  <Skeleton variant="rectangular" width={350} height={300} />
+                }
+                </Paper>
+              {courseData ?
+              <Box key={courseData.subject_code}>
+                <CourseSlider subject={courseData.subject_code} />
+              </Box>
+              : <Skeleton variant="rounded" width={300} height={150} />
+              }
             </Box>
           </Box>
         </Box>
