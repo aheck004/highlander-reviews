69c0708cabf8c4c552717d0f70e2aa0cdf59e44d
aheck004 <81645937+aheck004@users.noreply.github.com>
2023-08-24 02:19:51 -0700

    Added dark banner swap

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,18 +3,18 @@ import SearchBar from "./SearchBar";
 import "./hompage.css";
 import { Box, Button, Typography } from "@mui/material";
 import PrimarySearchAppBar from "./Header";
-import { ThemeProvider} from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import { themes } from "./themes";
 import Footer from "./footer";
 import { useTheme } from "./ThemeContext";
 import bannerLight from "../lightlogo1.svg";
-
+import bannerDark from "../darklogo1.svg";
 
 function HomePage() {
   const isMobile = window.innerWidth < 700;
 
   const theme = themes[useTheme().theme];
-  const setTheme = useTheme().toggleTheme; 
+  const setTheme = useTheme().toggleTheme;
 
   return (
     <ThemeProvider theme={theme}>
@@ -25,16 +25,36 @@ function HomePage() {
             display: "flex",
             flexDirection: "column",
             justifyContent: "flex-start",
-            marginTop: isMobile ? "32%" :"7%",
+            marginTop: isMobile ? "32%" : "7%",
             alignItems: "center",
             flexGrow: 1,
           }}
         >
           <Box sx={{ marginBottom: "20px" }}>
-            <Box sx={{display:'flex', marginBottom: "15px"}}>
-            <img src={bannerLight} className="App-logo" style={{ width: isMobile ? '95%': '60%', height: 'auto', marginLeft: "auto", marginRight:"auto", justifyContent: "center", alignSelf: "center"}} alt="logo" />
+            <Box sx={{ display: "flex", marginBottom: "15px" }}>
+              <img
+                src={
+                  theme.palette.name.main === "light" ? bannerLight : bannerDark
+                }
+                className="App-logo"
+                style={{
+                  width: isMobile ? "95%" : "60%",
+                  height: "auto",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  justifyContent: "center",
+                  alignSelf: "center",
+                }}
+                alt="logo"
+              />
             </Box>
-            <Typography className= "subtitle" variant="subtitle1" align="center" color="text.main" fontSize={isMobile ? 12 : 16}>
+            <Typography
+              className="subtitle"
+              variant="subtitle1"
+              align="center"
+              color="text.main"
+              fontSize={isMobile ? 12 : 16}
+            >
               Find reviews for courses at University of California, Riverside
             </Typography>
           </Box>
