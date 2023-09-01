d9e73aa13d98d1f7f50a0bc6d393b0155b114e57
jalvarez957 <87673004+KilloPillers@users.noreply.github.com>
2023-09-01 14:35:10 -0700

    ClassPage now has a redirect button when a course is cross-listed
    -minor changes to sx properties in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,25 +10,85 @@ function Footer() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{display:"flex", width:"100%", background:theme.palette.secondary.main, gap: "5%"}}>
-        <BioSquare 
-          name="Juan Alvarez"
-          title="Web Developer"
-          description="One of the developers of this website... and of all time"
-          image="https://media.licdn.com/dms/image/C5603AQHoGuMhIzIQUQ/profile-displayphoto-shrink_400_400/0/1633379824652?e=1695859200&v=beta&t=SdVycHyBY6ybgO8Otej1A1VwRz2ampTFZ3FsJTqhP5Q"
-          linkedin="https://www.linkedin.com/in/juan-alvarez-83250b1b9/"
-          github="https://github.com/KilloPillers"
-          flag="us"
-        />
-        <BioSquare 
-          name="Arthur Hecker"
-          title="Web Developer"
-          description="Developer of Highlander Reviews and many more to come"
-          image="https://media.licdn.com/dms/image/D5603AQGgvBYO9mk7Hw/profile-displayphoto-shrink_400_400/0/1691143938268?e=1696464000&v=beta&t=7LlX22qNJfWhIrQjxYBAWuT9li4CxkUek4n7pEHiAjw"
-          github="https://github.com/aheck004"
-          linkedin="https://www.linkedin.com/in/arthur-hecker-a56663230"
-          flag="us"
-        />
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          background: theme.palette.primary.main,
+          gap: "5%",
+          flexDirection: "row",
+        }}
+      >
+        <Link href="/">
+          <img
+            src={
+              theme.palette.name.main === "light"
+                ? smallBannerLight
+                : smallBannerBlue
+            }
+            style={{
+              paddingTop: "1dvh",
+              paddingBottom: "1dvh",
+              paddingLeft: "25%",
+              width: "5dvh",
+              height: "auto",
+            }}
+            alt="logo"
+          />
+        </Link>
+        <Box 
+          sx={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "flex-end",
+            flexDirection: isMobile ? "column" : "row",
+            width: "100%",
+            gap: "5%",
+            paddingTop: isMobile ? "1dvh" : "0dvh",
+            paddingBottom: isMobile ? "1dvh" : "0dvh",
+            paddingLeft: isMobile ? "8dvh" : "0dvh",
+            paddingRight: isMobile ? "8dvh" : "0dvh",
+          }}
+        >
+          <Link
+            href="#/About"
+            underline="hover"
+            color="inherit"
+            sx={{ color: "primary.contrastText" }}
+          >
+            <Typography fontSize={isMobile ? ".6rem" : ".9rem"} color="primary.contrastText">About</Typography>
+          </Link>
+          <Link
+            href="https://docs.google.com/forms/d/e/1FAIpQLSeZOC6LLk1OQ9Jctm3Pr_ZIJ4Vhio0ayoqJkkmzw1OjQhZzQg/viewform?usp=sf_link"
+            underline="hover"
+            color="inherit"
+            sx={{ color: "primary.contrastText" }}
+          >
+            <Typography fontSize={isMobile ? ".6rem" : ".9rem"} color="primary.contrastText">Feedback</Typography>
+          </Link>
+          <Link
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfvZsY3kayTVhC8gfwCsXk6JJhGUyzC8wAkm1V_s5c8yNDsPg/viewform?usp=sf_link"
+            underline="hover"
+            color="inherit"
+            sx={{ color: "primary.contrastText" }}
+          >
+            <Typography fontSize={isMobile ? ".6rem" : ".9rem"} color="primary.contrastText">Bug Report</Typography>
+          </Link>
+          <Link
+            href="https://github.com/aheck004/highlander-reviews"
+            underline="hover"
+            color="primary.contrastText"
+          >
+          <Typography
+              fontSize={isMobile ? ".6rem" : ".9rem"}
+              color="primary.contrastText"
+          >
+            GitHub
+          </Typography>
+          </Link>
+        </Box>
       </Box>
     </ThemeProvider>
   )
